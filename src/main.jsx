eaaae9b610c02b6e2feb0ce2469b539025e2b519
oliviadp <olivia.osypovych@gmail.com>
2025-01-16 21:45:17 +0200

    fin

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './reduce/store.js'
+import { store } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import { persistor } from './reduce/store.js'
+import { persistor } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
